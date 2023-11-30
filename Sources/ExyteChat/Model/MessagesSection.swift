412dc11a21ecd8d64824cb14e9859ad24c5ee0c6
Julien CAMPY <julien.campy@rolex.com>
2023-11-30 17:10:19 +0100

    Revert last commit

--- Sources/ExyteChat/Model/MessagesSection.swift
+++ Sources/ExyteChat/Model/MessagesSection.swift
@@ -2,17 +2,17 @@
 //  Created by Alex.M on 08.07.2022.
 //
 
-import SwiftUI
+import Foundation
 
 struct MessagesSection: Equatable {
 
-    @Environment(\.chatTheme) private var theme
-    
     let date: Date
     var rows: [MessageRow]
 
     static var formatter = {
-        return theme.customizations.conversationDateformatter
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, MMMM d"
+        return formatter
     }()
 
     init(date: Date, rows: [MessageRow]) {
